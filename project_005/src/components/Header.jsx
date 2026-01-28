992049a5e7e14a751c3ea32036ea51abcbf2c015
Seacrs <shemachris072@gmail.com>
2026-01-28 16:51:44 +0200

    add: create project_005

---
+++ project_005/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "../img/Logo.png"
+
+export default function Header(){
+    return (
+        <header>
+                <img src={chefClaudeLogo} alt="" />
+                <h1>Chef Claude</h1>
+        </header>
+    )
+}
