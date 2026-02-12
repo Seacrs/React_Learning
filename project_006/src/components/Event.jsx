d1156965126d777e8e79b842a9d0430abde2dac3
Seacrs <shemachris072@gmail.com>
2026-02-12 14:14:32 +0200

    feat: change inner workings of project

--- project_006/src/components/Event.jsx
+++ project_006/src/components/Event.jsx
@@ -1,15 +1,17 @@
 import React from "react"
+import Count from "../components/Count"
 
 export default function App() {
     const [count, setCount] = React.useState(0);
     const add = ()=> setCount((prevCount => prevCount + 1))
     const subtract = () => setCount((prevCount => prevCount - 1))
+    
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
                 <button onClick = {subtract} className="minus" aria-label="Decrease count">–</button>
-                <h2 className="count">{count}</h2>
+                <Count number = {count}/>
                 <button onClick={add} className="plus" aria-label="Increase count">+</button>
             </div>
         </main>
--- project_008/src/App.jsx
+++ project_008/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import avatar from "../img/avatar.png"
-import starFilled from "../img/star-filled.jpg"
-import starEmpty from "../img/star-empty.jpg"
+import Star from "./components/Star.jsx"
 
 export default function App() {
     const [contact, setContact] = React.useState({
@@ -11,39 +10,27 @@ export default function App() {
         email: "itsmyrealname@example.com",
         isFavorite: true
     })
-    
-    let starIcon = contact.isFavorite ? starFilled : starEmpty
 
     function toggleFavorite() {
-      setContact(prev => {
-        return {
-          ...prev,
-          isFavorite: !prev.isFavorite
-        }
-      })
+        setContact(prev => {
+            return {
+            ...prev,
+            isFavorite: !prev.isFavorite
+            }
+        })
     }
 
     return (
         <main>
             <article className="card">
+                
                 <img
                     src={avatar}
                     className="avatar"
                     alt="User profile picture of John Doe"
-                />
+        />
                 <div className="info">
-                    <button
-                        onClick={toggleFavorite}
-                        aria-pressed={contact.isFavorite}
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-                            className="favorite"
-                        />
-                    </button>
+                    <Star isFilled ={contact.isFavorite} handleClick={toggleFavorite}/>
                     <h2 className="name">
                         {contact.firstName} {contact.lastName}
                     </h2>
