689d42a373e3de6219ac7aacd03ec6622ace0068
Seacrs <shemachris072@gmail.com>
2026-02-14 20:10:33 +0200

    feat: update useEffect to fetch character based on count

--- project_013/src/App.jsx
+++ project_013/src/App.jsx
@@ -2,21 +2,19 @@ import React from "react"
 
 export default function App() {
     const [starWarsData, setStarWarsData] = React.useState({})
-    const [count, setCount] = React.useState(0)
-    
-    console.log("Rendered!")
-    
-    React.useEffect(function() {
-        console.log("Effect ran")
-        fetch("https://swapi.dev/api/people/1")
-            .then(res => res.json())
-            .then(data => setStarWarsData(data))
+    const [count, setCount] = React.useState(1)
+
+    React.useEffect(()=>{
+      fetch(`https://swapi.dev/api/people/${count}`)
+          .then(res => res.json())
+          .then(data => setStarWarsData(data))
     }, [count])
+    
     
     return (
         <div>
             <h2>The count is {count}</h2>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get next character</button>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
