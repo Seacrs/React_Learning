cdfdd25992e6117081d283e26b97c15dfb57a73a
Seacrs <shemachris072@gmail.com>
2026-02-16 16:11:39 +0200

    feat: persist button state when held

--- Capstone_Projects/Tenzies/src/App.jsx
+++ Capstone_Projects/Tenzies/src/App.jsx
@@ -4,6 +4,17 @@ import { nanoid } from 'nanoid'
 
 export default function App(){
 
+  /**
+     * Challenge: Create a function `hold` that takes
+     * `id` as a parameter. For now, just have the function
+     * console.log(id).
+     * 
+     * Then, figure out how to pass that function down to each
+     * instance of the Die component so when each one is clicked,
+     * it logs its own unique ID property. (Hint: there's more
+     * than one way to make that work, so just choose whichever
+     * you want)
+     */
   function generateAllNewDice(){
         return new Array(10)
                     .fill(0)
@@ -16,13 +27,25 @@ export default function App(){
 
     const [dice, setDice] = useState(generateAllNewDice())
 
-    const diceElements = dice.map((die)=><Die key={die.id} value = {die.value} isHeld={die.isHeld}/>)
+    function hold(id){
+      setDice(prev=>prev.map(item=> item.id === id ? {...item, isHeld: !item.isHeld} :item))
+  }
+
+    const diceElements = dice.map((die)=><Die  
+                                            key={die.id} 
+                                            value = {die.value} 
+                                            isHeld={die.isHeld}
+                                            hold={()=> hold(die.id)}/>
+)
 
     function rollDice(){
-        setDice(generateAllNewDice())
+      setDice(prev=>prev.map(item=> item.isHeld ? item : { ...item, value: Math.ceil(Math.random() * 6)}))
     }
+  
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="container">
         {diceElements}
       </div>
--- Capstone_Projects/Tenzies/src/components/Die.jsx
+++ Capstone_Projects/Tenzies/src/components/Die.jsx
@@ -2,7 +2,8 @@ export default function Die(props){
     const styles = {
         backgroundColor: props.isHeld && '#59E391'
     }
+
     return (
-        <button style={styles} className="dice-button">{props.value}</button>
+        <button onClick={props.hold} style={styles} className="dice-button">{props.value}</button>
     )
 }
