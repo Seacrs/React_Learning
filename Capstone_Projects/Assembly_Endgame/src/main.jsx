f2a0f6d000e27cd1fec8ffaa421962a8a78dd491
Seacrs <shemachris072@gmail.com>
2026-02-18 10:45:38 +0200

    feat: Initialize repository

---
+++ Capstone_Projects/Assembly_Endgame/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import AssemblyEndgame from './App'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+    <AssemblyEndgame />
+)
