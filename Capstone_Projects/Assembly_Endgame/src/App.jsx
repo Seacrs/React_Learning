cb2ccb5a9421bf421699bbc5fce21cbe34338935
Seacrs <shemachris072@gmail.com>
2026-02-18 11:47:40 +0200

    feat: build main UI structure for Assembly Endgame

--- Capstone_Projects/Assembly_Endgame/src/App.jsx
+++ Capstone_Projects/Assembly_Endgame/src/App.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-
-/**
- * Goal: Build out the main parts of our app
- * 
- * Challenge: Build a status section below the header.
- * For now, you can just hard-code in the styles for
- * a winning game, and we'll make it more dynamic
- * later.
- */ 
+import { useState, useEffect } from 'react'
+import { languages } from './languages.js'
 
 export default function AssemblyEndgame(){
+
+    const [currentWord, setCurrentWord] = useState('react')
+
+    const letterElements = currentWord.split('').map(letter=> <span>{letter.toUpperCase()}</span>)
+    
+const languageList = languages.map(language=><span      className='chip'
+                                                        key={language.name}
+                                                        style={{
+                                                            backgroundColor:language.backgroundColor,
+                                                            color: language.color
+                                                        }}>{language.name}</span>)
     return (
         <main>
             <header>
                 <h1>Assembly EndGame</h1>
                 <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
             </header>
+            <section className='status'>
+                    <h2>You win!</h2>
+                    <p>Well Done!🎉</p>
+            </section>
+            <section className='language_section'>
+                    {languageList}
+            </section>
+            <section className='word'>
+                {letterElements}
+            </section>
         </main>
     )
 }
