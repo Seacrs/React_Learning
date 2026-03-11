97179b79894def4d323d6fca98a2a5c4a4813234
Seacrs <shemachris072@gmail.com>
2026-03-11 21:29:16 +0200

    feat: add searchable product list with useMemo and useCallback

--- project_017/src/App.jsx
+++ project_017/src/App.jsx
@@ -1,15 +1,44 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo, useCallback } from 'react'
 
 export default function App(){
 
-  // Build a React component that:
+  const [items, setItems] = useState([])
+  const [input, setInput] = useState('')
 
-  // Renders a list of 200 items
-  // Includes a text input field to filter displayed items
-  // Optimizes performance using React hooks
+  useEffect(()=> {
+    fetch('https://api.escuelajs.co/api/v1/products')
+        .then(res => res.json())
+        .then(data => setItems(data.slice(0, 200)))
+  }, [])
 
-  
+  const getText = useCallback((e)=>{
+    setInput(e.target.value)
+  }, [])
+
+    const filteredItems = useMemo(()=>{
+      if(items.length === 0 || !input){
+        return items;
+      }
+      const newData = items.filter(el => el.title.toLowerCase().includes(input.toLowerCase()))
+
+      return newData;
+
+    }, [input, items])
+
+    const listItems = filteredItems.map(item => {
+    return (
+      <div key={item.id} className='p-2'>
+        <p>{item.title}</p>
+      </div>
+    )
+  })
+
   return (
-    <h1>App Component</h1>
+    <div className='p-10'>
+      <input type="text" name='filter' className='border-2 py-1 pl-5' onChange={getText}/>
+      <div className='p-3 flex flex-col gap-4'>
+        {listItems}
+      </div>
+    </div>
   )
 }
