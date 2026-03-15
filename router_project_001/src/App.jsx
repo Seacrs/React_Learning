444bfbabddb16dd368f8e0831d5c02108db679c7
Seacrs <shemachris072@gmail.com>
2026-03-15 18:03:27 +0200

    refactor: migrate to createBrowserRouter and RouterProvider

--- router_project_001/src/App.jsx
+++ router_project_001/src/App.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Outlet }  from "react-router-dom"
+import { 
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route
+}    from "react-router-dom"
+
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -18,10 +23,7 @@ import NotFound from './pages/NotFound'
 
 import "./server"
 
-export default function App(){
-    return (
-        <BrowserRouter>
-            <Routes>
+const router = createBrowserRouter(createRoutesFromElements(
                 <Route path='/' element={<Layout />}>
                     <Route index element={<Home />}/>
                     <Route path="about" element={<About />}/>
@@ -41,7 +43,10 @@ export default function App(){
                     </Route>
                     <Route path = "*" element={<NotFound />}/>
                 </Route>
-            </Routes>
-        </BrowserRouter>
+))
+
+export default function App(){
+    return (
+        <RouterProvider router = {router}/>
     )
 }
