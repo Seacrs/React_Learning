96cad4db0bf417b00ba7772f67120a443d765e25
Seacrs <shemachris072@gmail.com>
2026-03-31 22:19:59 +0200

    feat: add redux implementation project

---
+++ project_024/src/App.jsx
@@ -0,0 +1,12 @@
+import MovieList from "./components/MovieList"
+import MovieInput from "./components/MovieInput"
+
+export default function App(){
+  return (
+    <>
+    {" "}
+    <MovieInput />
+    <MovieList />
+    </>
+  )
+}
